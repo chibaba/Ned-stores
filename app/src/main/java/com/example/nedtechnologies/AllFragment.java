6e102114131aa6065fac7ff6409392f1db618979
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-03 01:42:03 +0100

    adding recycler view

--- app/src/main/java/com/example/nedtechnologies/AllFragment.java
+++ app/src/main/java/com/example/nedtechnologies/AllFragment.java
@@ -3,18 +3,21 @@ package com.example.nedtechnologies;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AllFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class AllFragment extends Fragment {
 
+    private RecyclerView allRecycler;
 
 
 
@@ -22,6 +25,14 @@ public class AllFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View myview = inflater.inflate(R.layout.fragment_all, container, false);
+
+        allRecycler = myview.findViewById(R.id.recycler_all);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        allRecycler.setHasFixedSize(true);
+        allRecycler.setLayoutManager(layoutManager);
 
         return  myview;
     }
