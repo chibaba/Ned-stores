5d65f93b2749a39d24daed3300e0bc7201b436eb
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-10 02:03:26 +0100

    wip

--- app/src/main/java/com/example/nedtechnologies/Model/Data.java
+++ app/src/main/java/com/example/nedtechnologies/Model/Data.java
@@ -17,4 +17,36 @@ public class Data {
         this.description = description;
         this.id = id;
     }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
