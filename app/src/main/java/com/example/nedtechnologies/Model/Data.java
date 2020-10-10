ce519aaf3e60829382806cffc45cb6d72249ba1b
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-10 02:01:54 +0100

    created constructor for the data model class

--- app/src/main/java/com/example/nedtechnologies/Model/Data.java
+++ app/src/main/java/com/example/nedtechnologies/Model/Data.java
@@ -1,4 +1,20 @@
 package com.example.nedtechnologies.Model;
 
 public class Data {
+
+    private String image;
+    private String title;
+    private String description;
+    private String id;
+
+    public Data() {
+
+    }
+
+    public Data(String image, String title, String description, String id) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+        this.id = id;
+    }
 }
